f26e19e67ff24cc458a5314dcd9aa0f78bd3aa21
Achu-nair <42400273+Achu-nair@users.noreply.github.com>
2019-03-02 15:18:03 +0530

    Add files via upload

---
+++ assignment_5_2.r
@@ -0,0 +1,16 @@
+#assignment 5.2
+
+vec1 = c(rownames(mtcars[1:15,]))
+vec2 = c(rownames(mtcars[10:32,]))
+
+#union of elements
+union(vec1,vec2)
+
+#elements that are common to both vectors
+intersect(vec1,vec2)
+
+#difference between two character vectors
+setdiff(vec1,vec2)
+
+#quality of vectors
+setequal(vec1,vec2)
